039fcb6851d962ad5bc5ec7e489c7d981eb59785
LucaLF-dev <laficaraluca@gmail.com>
2023-11-21 17:55:33 +0100

    Add - title and print text

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-
+$text = 'A bocce ferme, rimane probabile in primavera l’apertura di una procedura per deficit eccessivo (previsto al 4,4% del PIL nel 2024), quando verrà a scadere la sospensione delle regole di bilancio decisa al momento dello scoppio della pandemia. D’altro canto, ancora una volta, l’Italia è ritenuto tra i paesi segnati da uno squilibrio macroeconomico, per via dell’elevato debito pubblico e della bassa competitività. Nella stessa situazione sono la Germania, la Francia, l’Olanda e altri otto paesi.'
 
 ?>
 
@@ -13,6 +13,11 @@
   <title>PHP Badwords</title>
 </head>
 <body>
-  
+
+<h1> Manovra, da Bruxelles arriva un sì con riserva. Bocciata la Francia
+</h1>
+
+<p> <?php echo $text ?></p>
+
 </body>
 </html>
