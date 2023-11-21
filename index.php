39f08e5f1395aa2d3bd1f2386bd5de7344f79541
LucaLF-dev <laficaraluca@gmail.com>
2023-11-21 18:38:40 +0100

    Add - replace text  word and print new_text  to index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-$text = 'A bocce ferme, rimane probabile in primavera l’apertura di una procedura per deficit eccessivo (previsto al 4,4% del PIL nel 2024), quando verrà a scadere la sospensione delle regole di bilancio decisa al momento dello scoppio della pandemia. D’altro canto, ancora una volta, l’Italia è ritenuto tra i paesi segnati da uno squilibrio macroeconomico, per via dell’elevato debito pubblico e della bassa competitività. Nella stessa situazione sono la Germania, la Francia, l’Olanda e altri otto paesi.'
+$text = 'A bocce ferme, rimane probabile in primavera l’apertura di una procedura per deficit eccessivo (previsto al 4,4% del PIL nel 2024), quando verrà a scadere la sospensione delle regole di bilancio decisa al momento dello scoppio della pandemia. D’altro canto, ancora una volta, l’Italia è ritenuto tra i paesi segnati da uno squilibrio macroeconomico, per via dell’elevato debito pubblico e della bassa competitività. Nella stessa situazione sono la Germania, la Francia, l’Olanda e altri otto paesi.';
+
+$replace = $_POST['replace_word'];
+
+var_dump($_POST);
+
+$new_text = str_replace($replace,'***',$text);
 
 ?>
 
@@ -18,6 +24,12 @@ $text = 'A bocce ferme, rimane probabile in primavera l’apertura di una proced
 </h1>
 
 <p> <?php echo $text ?></p>
+
+<p> Lunghezza testo originale : <?php echo  strlen($text) ?> caratteri </p>
+
+<p> <?php echo str_replace($replace,'***',$text) ?></p>
+
+<p> Lunghezza testo originale : <?php echo  strlen($new_text) ?> caratteri </p>
 
 </body>
 </html>
