739f8ca6646c91f8cd7cc8a2b11c92e0fbd3cfb2
LucaLF-dev <laficaraluca@gmail.com>
2023-11-21 18:05:08 +0100

    Add - form to word.php

---
+++ word.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pagina Parola</title>
+</head>
+
+<body>
+
+  <form action="index.php" method="POST">
+    <input type="text" name="replace_word">
+    <input type="submit" value="Invia">
+  </form>
+
+</body>
+
+</html>
